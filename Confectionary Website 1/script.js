ee1aed5474dc415c266345416635d996201083e9
SaloniSinghal27 <133648443+SaloniSinghal27@users.noreply.github.com>
2024-08-21 22:15:26 +0530

    Add files via upload

---
+++ Confectionary Website 1/script.js
@@ -0,0 +1,36 @@
+// Toggle navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Scroll sections active
+let sections = document.querySelectorAll('section');
+let navbarLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navbarLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // Sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove navbar when click
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
